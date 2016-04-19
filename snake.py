86164cb944207c181441616bda1c9528d3129b42
codekavi <code.kavi@gmail.com>
2016-04-19 15:20:47 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -25,7 +25,7 @@ class SnakeGame(object):
         self.f = pygame.font.SysFont('Arial', 20)
         self.clock = pygame.time.Clock()
 
-
+//can you explain me the coordinates, how the condition is checked 
     def collide(self, x1, x2, y1, y2, w1, w2, h1, h2):
         if x1 + w1 > x2 and x1 < x2 + w2 and y1 + h1 > y2 and y1 < y2 + h2:
             return True
